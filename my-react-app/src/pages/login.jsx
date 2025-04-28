00d691c5a404150b45f2b26ab9275c9aed6a185d
Conner Benitez <conner.benitez27@houghton.edu>
2025-04-28 10:38:01 -0400

    Login Frontend

---
+++ my-react-app/src/pages/login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './login.css';
+
+function Login() {
+  return (
+    <div className="gradient-form">
+      <div className="card-container">
+        <div className="login-left">
+          <div className="logo-container">
+            <img src="./images/logo.jpg" alt="logo" className="logo" />
+            
+          </div>
+          <p>Please login to your account</p>
+          <input type="email" placeholder="Email address" className="input-field" />
+          <input type="password" placeholder="Password" className="input-field" />
+          <button className="btn btn-primary">Sign in</button>
+          <a href="#!" className="forgot-password">Forgot password?</a>
+          <div className="signup-container">
+            <p>Don't have an account?</p>
+            <button className="btn btn-danger">Sign up</button>
+          </div>
+        </div>
+
+        <div className="login-right">
+          <h4>Keep Track of Your Mental Health Progress</h4>
+          <p>Monitor your mental health journey, set goals, and celebrate every victory along the way.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
