b3c28ebb6efd1f98e6b45b5f6339a3021e407427
ashg555 <ashleigh.gossman26@houghton.edu>
2025-05-03 10:25:46 -0400

    Edited localStorage to save entries

--- MentalHealth-main/my-react-app/src/pages/View.jsx
+++ MentalHealth-main/my-react-app/src/pages/View.jsx
@@ -1,12 +1,21 @@
 import NavBar from "../components/NavBar";
 
 export default function View() {
+  // get Entry data from localStorage
+  const loggedInUserEmail = JSON.parse(localStorage.getItem("authToken")).email;
+  const storedUsers = JSON.parse(localStorage.getItem("users") || "[]");
+  const user = storedUsers.find((u) => u.email === loggedInUserEmail);
 
-  return <>
+  // note: entries are stored in this format: date1': {entry}, 'date2': {entry}
+  const entries = user.entries;
 
-    <NavBar/>
-    <h1>Todo: View Entries</h1>
-  
-  </>
+  console.log(entries);
 
+  return (
+    <>
+      <NavBar />
+    <h1>Your Entries</h1>
+    <p>{JSON.stringify(entries)}</p>
+    </>
+  );
 }
