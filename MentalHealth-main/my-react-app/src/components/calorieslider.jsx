39b26ef795eda16f7455722bb4c5df935918f96b
SarahWinne <sarah.winne27@houghton.edu>
2025-05-05 10:02:06 -0400

    adjusted calories and sleep bounds

--- MentalHealth-main/my-react-app/src/components/calorieslider.jsx
+++ MentalHealth-main/my-react-app/src/components/calorieslider.jsx
@@ -5,8 +5,8 @@ const CaloricIntakeSlider = ({ value, setValue }) => (
     <h2 className="text-xl font-bold mb-2">Caloric Intake</h2>
     <input
       type="range"
-      min="1000"
-      max="4000"
+      min="0"
+      max="5000"
       step="100"
       value={value}
       onChange={(e) => setValue(Number(e.target.value))}
