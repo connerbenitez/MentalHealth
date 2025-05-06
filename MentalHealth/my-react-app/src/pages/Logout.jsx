ac9328a170e6b71362e9a0a0a2cbfa88d515daf3
SarahWinne <sarah.winne27@houghton.edu>
2025-05-05 23:11:27 -0400

    Test + removing unsucessful one

--- MentalHealth/my-react-app/src/pages/Logout.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function Logout() {
-  //on logout: delete the auth token, then render the login page
-
-  localStorage.removeItem("authToken");
-  return <Navigate to="/" replace />;
-}
